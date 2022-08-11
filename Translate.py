3b8e7ef90075534b5d2f5c67dc32899c1ade6d39
Fraqtio <fraqtio.invektiko@gmail.com>
2022-08-11 13:48:57 +0300

    Translate adding

---
+++ Translate.py
@@ -0,0 +1,19 @@
+weekdays = {'Понедельник': 'Monday',
+            'Вторник': 'Tuesday',
+            'Среда': 'Wednesday',
+            'Четверг': 'Thursday',
+            'Пятница': 'Friday',
+            'Суббота': 'Saturday',
+            'Воскресенье': 'Sunday',
+            }
+weather_effect =   {'облачно с прояснениями': 'cloudy with clearings',
+                    'сильный дождь': 'heavy rain',
+                    'переменная облачность': 'partly cloudy',
+                    'дождь': 'rain',
+                    'возможны грозы': 'thunderstorms possible',
+                    'мелкий дождь': 'light rain',
+                    'небольшая облачность': 'partly cloudy',
+                    'сплошная облачность': 'overcast',
+                    'грозы': 'thunderstorms',
+                    'ясно': 'clear'
+                    }
